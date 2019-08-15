6288cf77b982f5763e169acf464a2397f1506ab5
Jeff Long <jefflong94@gmail.com>
2019-08-15 12:35:11 -0700

    Fixed test class name

--- test/controllers/conversation_controller_test.rb
+++ test/controllers/conversation_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
-class ChatControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get chat_show_url
-    assert_response :success
-  end
-
+class ConversationsControllerTest < ActionDispatch::IntegrationTest
 end
