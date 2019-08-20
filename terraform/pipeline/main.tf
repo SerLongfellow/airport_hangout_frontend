a3e02f2e7d0f91b202a6f3c03f7a88cfdef071a8
Jeff Long <jefflong94@gmail.com>
2019-08-20 16:20:29 -0700

    More terraform and test new webhook

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+variable "webhook_secret" {
+  type        = string
+  description = "Secret to integrate GitHub webhook into CodePipeline"
+}
+
 terraform {
   backend "s3" {
     bucket = "com.jefflong.airporthangout.ops"
@@ -123,23 +128,6 @@ resource "aws_codebuild_project" "codebuild" {
   }
 }
 
-resource "aws_codebuild_webhook" "codebuild_webhook" {
-  project_name = aws_codebuild_project.codebuild.name
-  filter_group {
-    filter {
-      type    = "EVENT"
-      pattern = "PUSH"
-    }
-  }
-
-  filter_group {
-    filter {
-      type    = "EVENT"
-      pattern = "PULL_REQUEST_CREATED,PULL_REQUEST_UPDATED,PULL_REQUEST_REOPENED,PULL_REQUEST_MERGED"
-    }
-  }
-}
-
 resource "aws_codedeploy_app" "codedeploy_app" {
   compute_platform = "ECS"
   name             = "airport_hangout_frontend_app"
@@ -154,3 +142,123 @@ resource "aws_codedeploy_deployment_config" "codedeploy_config" {
   }
 }
 
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket_prefix = "com.jefflong.pipelines"
+  acl           = "private"
+  force_destroy = true
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy"
+  role = "${aws_iam_role.codepipeline_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.codepipeline_bucket.arn}",
+        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_codepipeline" "codepipeline" {
+  name      = "airport_hangout_frontend_pipeline"
+  role_arn  = "${aws_iam_role.codepipeline_role.arn}"
+  
+  artifact_store {
+    location = "${aws_s3_bucket.codepipeline_bucket.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name        = "Source"
+    
+    action {
+      name        = "Source"
+      category    = "Source"
+      owner       = "ThirdParty"
+      provider    = "GitHub"
+      version     = "1"
+      output_artifacts = ["SourceArtifact"]
+
+      configuration = {
+        Owner  = "SerLongfellow"
+        Repo   = "airport_hangout_frontend"
+        Branch = "feature/ci"
+        PollForSourceChanges = "false"
+      }
+    }
+  }
+  
+  stage {
+    name        = "Build"
+    
+    action {
+      name        = "Build"
+      category    = "Build"
+      owner       = "AWS"
+      provider    = "CodeBuild"
+      version     = "1"
+      input_artifacts = ["SourceArtifact"]
+      output_artifacts = ["BuildArtifact"]
+
+      configuration = {
+        ProjectName = "airport_hangout_frontend"
+      }
+    }
+  }
+}
+
+/*
+resource "aws_codepipeline_webhook" "codepipeline_webhook" {
+  name = "airport_hangout_frontend_codepipeline_github_webhook"
+  target_pipeline = "${aws_codepipeline.codepipeline.name}"
+  target_action   = "Source"
+
+  authentication = "GITHUB_HMAC"
+  authentication_configuration {
+    secret_token = "${var.webhook_secret}"
+  }
+   
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/heads/{Branch}"
+  }
+}*/
