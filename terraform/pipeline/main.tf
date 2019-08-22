56ab9eb098a519976fd5cc5628ece67cc4a31162
Jeff Long <jefflong94@gmail.com>
2019-08-21 17:08:28 -0700

    more tweaks

--- terraform/pipeline/main.tf
+++ terraform/pipeline/main.tf
@@ -3,16 +3,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-variable "webhook_secret" {
-  type        = string
-  description = "Secret to integrate GitHub webhook into CodePipeline"
-}
-
-variable "github_token" {
-  type        = string
-  description = "Token to use GitHub API from CodePipeline"
-}
-
 variable "ops_bucket" {
   type        = string
   description = "Name of bucket used for terraform state storage - needed to provision CodeBuild infrastructure job permissions"
