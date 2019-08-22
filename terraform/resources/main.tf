9e2f0d65d2a339d89b31c051fdacb8735d27977e
Jeff Long <jefflong94@gmail.com>
2019-08-21 18:17:57 -0700

    made bucket versionable

--- terraform/resources/main.tf
+++ terraform/resources/main.tf
@@ -3,6 +3,10 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket        = "com.jefflong.pipelines"
   acl           = "private"
   force_destroy = true
+
+  versioning {
+    enabled = true
+  }
 }
 
 output "codepipeline_bucket_output" {
